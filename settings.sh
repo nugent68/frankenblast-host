60fe41588df7aa23e9db64632b4fb5319fe63f5a
Anya Nugent <anugent96@gmail.com>
2025-08-19 12:04:42 -0400

    Create settings.sh

---
+++ settings.sh
@@ -0,0 +1,5 @@
+export SPS_HOME=/opt/anaconda3/envs/prospector_env/lib/python3.10/site-packages/fsps/
+export SBIPP_TRAINING_ROOT=/Users/anyanugent/Documents/GitHub/frankenblast/data/sbi_training_sets
+export SED_OUTPUT_ROOT=/Users/anyanugent/Documents/GitHub/frankenblast/data/sed_output
+export SBIPP_ROOT=/Users/anyanugent/Documents/GitHub/frankenblast/data/SBI
+export SBIPP_PHOT_ROOT=/Users/anyanugent/Documents/GitHub/frankenblast/data/sbipp_phot
