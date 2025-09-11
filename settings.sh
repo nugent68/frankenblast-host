5ee89dfd5499c1db503c19194d576b7caf34ae32
Anya Nugent <anugent96@gmail.com>
2025-09-11 17:28:17 -0400

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,6 +1,6 @@
-export SPS_HOME=/opt/anaconda3/envs/frankenblast-hosts/lib/python3.10/site-packages/fsps/
-export SBIPP_TRAINING_ROOT=/Users/anyanugent/Documents/GitHub/frankenblast/data/sbi_training_sets
-export SED_OUTPUT_ROOT=/Users/anyanugent/Documents/GitHub/frankenblast/data/sed_output
-export SBIPP_ROOT=/Users/anyanugent/Documents/GitHub/frankenblast/data/SBI
-export SBIPP_PHOT_ROOT=/Users/anyanugent/Documents/GitHub/frankenblast/data/sbipp_phot
-export PROST_PATH=/opt/anaconda3/envs/frankenblast-hosts/lib/python3.10/site-packages/astro_prost/data
+export SPS_HOME=/your/path/to/fsps/
+export SBIPP_TRAINING_ROOT=/your/path/to/frankenblast/data/sbi_training_sets
+export SED_OUTPUT_ROOT=/your/path/to/frankenblast/data/sed_output
+export SBIPP_ROOT=/your/path/to/frankenblast/data/SBI
+export SBIPP_PHOT_ROOT=/your/path/to/frankenblast/data/sbipp_phot
+export PROST_PATH=/your/path/to/astro_prost/data
